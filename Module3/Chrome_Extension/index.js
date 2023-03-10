2cf1ca46825f96c089f0f7942dd735a51b98d4f4
Jeff Hamill <jthamill@gmail.com>
2023-03-10 15:49:38 -0500

    Using temperate literals in the rendering of the innerHTML

--- Module3/Chrome_Extension/index.js
+++ Module3/Chrome_Extension/index.js
@@ -1,4 +1,24 @@
-let inputBtn = document.getElementById("input-btn");
-inputBtn.addEventListener("click", () => {
-  console.log("Button clicked via event listener");
-});
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // Wrap the lead in an anchor tag (<a>) inside the <li>
+        // Can you make the link open in a new tab?
+        // listItems += '<li><a target="_blank" href=' +myLeads[i] + '>' + myLeads[i] + '</a></li>'
+        listItems += `
+        <li>
+          <a target="_blank" href="${myLeads[i]}">${myLeads[i]}</a>
+        </li>`
+    }
+    ulEl.innerHTML = listItems  
+}
