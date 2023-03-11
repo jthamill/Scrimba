ee196853463dcd3d6d4569e73e5edca44c3b5d86
Jeff Hamill <jthamill@gmail.com>
2023-03-10 20:29:56 -0500

    added delete button and functionality to reset local storage to null

--- Module3/Chrome_Extension/index.js
+++ Module3/Chrome_Extension/index.js
@@ -1,6 +1,7 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 console.log(leadsFromLocalStorage);
@@ -10,12 +11,18 @@ if (leadsFromLocalStorage) {
 renderLeads();
 
 
-inputBtn.addEventListener("click", function() {
+inputBtn.addEventListener("click", () => {
     myLeads.push(inputEl.value);
     inputEl.value = "";
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
     renderLeads();
     console.log(localStorage.getItem("myLeads"));
+})
+
+deleteBtn.addEventListener("click", () =>{
+  localStorage.setItem("myLeads", null);
+  myLeads = [];
+  renderLeads();
 })
 
 function renderLeads() {
@@ -28,6 +35,7 @@ function renderLeads() {
         <li>
           <a target="_blank" href="${myLeads[i]}">${myLeads[i]}</a>
         </li>`;
+        // re-factored with string literal
     }
     ulEl.innerHTML = listItems;  
 }
