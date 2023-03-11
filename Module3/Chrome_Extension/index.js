9030db476bfc81d44176f4bce799fcf371af4671
Jeff Hamill <jthamill@gmail.com>
2023-03-10 21:20:00 -0500

    refactored and re-named the render function. It now takes a param for the initial array

--- Module3/Chrome_Extension/index.js
+++ Module3/Chrome_Extension/index.js
@@ -4,38 +4,40 @@ const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-console.log(leadsFromLocalStorage);
+
+
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
 }
-renderLeads();
+render(myLeads);
+
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+      // Wrap the lead in an anchor tag (<a>) inside the <li>
+      // Can you make the link open in a new tab?
+      // listItems += '<li><a target="_blank" href=' +myLeads[i] + '>' + myLeads[i] + '</a></li>'
+      listItems += `
+      <li>
+        <a target="_blank" href="${leads[i]}">${leads[i]}</a>
+      </li>`;
+      // re-factored with string literal
+  }
+  ulEl.innerHTML = listItems;  
+}
 
 
 inputBtn.addEventListener("click", () => {
     myLeads.push(inputEl.value);
     inputEl.value = "";
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    renderLeads();
+    render(myLeads);
     console.log(localStorage.getItem("myLeads"));
 })
 
 deleteBtn.addEventListener("dblclick", () =>{
-  localStorage.setItem("myLeads", null);
+  localStorage.clear();
   myLeads = [];
-  ulEl.innerHTML = null;
+  render(myLeads);
 })
-
-function renderLeads() {
-    let listItems = "";
-    for (let i = 0; i < myLeads.length; i++) {
-        // Wrap the lead in an anchor tag (<a>) inside the <li>
-        // Can you make the link open in a new tab?
-        // listItems += '<li><a target="_blank" href=' +myLeads[i] + '>' + myLeads[i] + '</a></li>'
-        listItems += `
-        <li>
-          <a target="_blank" href="${myLeads[i]}">${myLeads[i]}</a>
-        </li>`;
-        // re-factored with string literal
-    }
-    ulEl.innerHTML = listItems;  
-}
