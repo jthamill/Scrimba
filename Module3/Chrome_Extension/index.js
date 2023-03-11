4bc3b6fbc264de1601e8ec4b52fb185872a05cd5
Jeff Hamill <jthamill@gmail.com>
2023-03-10 20:12:12 -0500

    Added local storage functionality

--- Module3/Chrome_Extension/index.js
+++ Module3/Chrome_Extension/index.js
@@ -1,16 +1,25 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+}
+renderLeads();
+
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    renderLeads();
+    console.log(localStorage.getItem("myLeads"));
 })
 
 function renderLeads() {
-    let listItems = ""
+    let listItems = "";
     for (let i = 0; i < myLeads.length; i++) {
         // Wrap the lead in an anchor tag (<a>) inside the <li>
         // Can you make the link open in a new tab?
@@ -18,7 +27,7 @@ function renderLeads() {
         listItems += `
         <li>
           <a target="_blank" href="${myLeads[i]}">${myLeads[i]}</a>
-        </li>`
+        </li>`;
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems;  
 }
