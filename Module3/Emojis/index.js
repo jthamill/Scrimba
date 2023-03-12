8a1f04cd21ebfc3f2f1e30fc9c207cdd17e40495
Jeff Hamill <jthamill@gmail.com>
2023-03-11 23:12:54 -0500

    bsaic layout for bball scoreboard

--- Module3/Emojis/index.js
+++ Module3/Emojis/index.js
@@ -1,45 +1,64 @@
-const myEmojis = ["👨‍💻", "⛷", "🍲"]
+// const myEmojis = ["👨‍💻", "⛷", "🍲"];
+const myEmojis = ["👨‍💻", "⛷", "🍲"];
 
-function renderEmojis() {
-    const emojiContainer = document.getElementById("emoji-container")
-    emojiContainer.innerHTML = ""
-    for (let i = 0; i < myEmojis.length; i++) {
-        const emoji = document.createElement('span')
-        emoji.textContent = myEmojis[i]
-        emojiContainer.append(emoji)
-    }
+const emojiContainer = document.getElementById("emoji-container");
+const emojiInput = document.getElementById("emoji-input")
+const pushBtn = document.getElementById("push-btn");
+const unshiftBtn = document.getElementById("unshift-btn")
+const popBtn = document.getElementById("pop-btn")
+const shiftBtn = document.getElementById("shift-btn")
+
+
+function renderEmojis(array) {
+  emojiContainer.innerHTML = "";
+  emojiInput.value = "";
+  for (let i = 0; i < myEmojis.length; i++) {
+    const emoji = document.createElement('span');
+    emoji.textContent = myEmojis[i];
+    emojiContainer.append(emoji);
+  }
 }
 
-renderEmojis()
+// partial re-factor. Just a test
 
-const pushBtn = document.getElementById("push-btn")
+function modifyEmojis(job) {
+  if (job = "push") {
+    myEmojis.push(emojiInput.value);
+    renderEmojis(myEmojis);
+  }
+}
+
+
+
 pushBtn.addEventListener("click", function(){
-    const emojiInput = document.getElementById("emoji-input")
-    if (emojiInput.value) {
-        myEmojis.push(emojiInput.value)
-        emojiInput.value = ""
-        renderEmojis()
-    }
-})
+  if (emojiInput.value) {
+    // myEmojis.push(emojiInput.value);
+    // emojiInput.value = "";
+    // renderEmojis();
+    modifyEmojis("push");
+  }
+});
 
-const unshiftBtn = document.getElementById("unshift-btn")
+
 unshiftBtn.addEventListener("click", function(){
-    const emojiInput = document.getElementById("emoji-input")
+    const emojiInput = document.getElementById("emoji-input");
     if (emojiInput.value) {
-        myEmojis.unshift(emojiInput.value)
-        emojiInput.value = ""
-        renderEmojis()
+        myEmojis.unshift(emojiInput.value);
+        emojiInput.value = "";
+        renderEmojis();
     }
-})
+});
 
-const popBtn = document.getElementById("pop-btn")
+
 popBtn.addEventListener("click", function(){
-    myEmojis.pop()
-    renderEmojis()
-})
+  myEmojis.pop();         
+  renderEmojis();
+});
 
-const shiftBtn = document.getElementById("shift-btn")
+
 shiftBtn.addEventListener("click", function(){
-    myEmojis.shift()
-    renderEmojis()
-})
+  myEmojis.shift();
+  renderEmojis();
+});
+
+renderEmojis(myEmojis);
